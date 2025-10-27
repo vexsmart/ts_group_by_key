d5c2f240c322ee1ec1105a378d8d08094604a6c7
Vitor de Freitas Ribeiro <programador.vitor.ribeiro@hotmail.com>
2025-10-27 19:11:58 -0300

    task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const groups: any = {};
+
+  items.forEach((item) => {
+    const propertyValue = (item as { [k: string]: T })[key];
+
+    if (!groups[propertyValue]) {
+      groups[propertyValue] = [];
+    }
+
+    groups[propertyValue].push(item);
+  });
+
+  return groups as GroupsMap<T>;
 }
